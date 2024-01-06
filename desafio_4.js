6a05787427b4ce7df0ac2b0a077689f2d6e375ad
Regis <regisuniverso@gmail.com>
2024-01-06 15:41:58 -0300

    terminando o desafio

--- desafio_4.js
+++ desafio_4.js
@@ -1,11 +1,39 @@
 /* 1. Crie uma lista vazia, com o nome listaGenerica.*/
 
+let listaGenerica = [];
+
+console.log(listaGenerica);
+
+
 /* 2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'. */
+
+let linguagensDeProgramacao = ['JavaScript','C','C++','Kotlin','Python'];
+
+console.log(linguagensDeProgramacao);
+
 
 /* 3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'. */
 
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+console.log(linguagensDeProgramacao);
+
+
 /* 4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento. */
+
+let listaDeTresNomes = ['Ana', 'Jair', 'Ari'];
+
+console.log(listaDeTresNomes[0]);
+
 
 /* 5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento. */
 
+console.log(listaDeTresNomes[1]);
+
 /* 6. Crie uma lista com 3 nomes e exiba no console apenas o último elemento. */
+
+console.log(listaDeTresNomes[2]);
+
+console.log(listaDeTresNomes);
